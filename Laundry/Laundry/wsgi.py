51217ffe9da10b5438048ea333b5c57aa7ef33a5
Ermias80 <ermiasmaz8069@gmail.com>
2025-06-03 21:27:38 +0300

    for deploy

--- Laundry/Laundry/wsgi.py
+++ Laundry/Laundry/wsgi.py
@@ -10,7 +10,11 @@ https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Laundry.settings')
+settings_module = (
+    'Laundry.deployment_Settings'
+    if 'RENDER_EXTERNAL_HOSTNAME' in os.environ
+    else 'Laundry.settings'
+)
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'settings_module')
 
 application = get_wsgi_application()
---
+++ Laundry/build.sh
@@ -0,0 +1,4 @@
+Set -o errexit
+pip install -r requirements.txt
+python manage.py collectstatic --no-input
+python manage.py migrate
