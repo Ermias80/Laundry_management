9276a481e24bd330e4588625100fb131ce8aa018
Ermias80 <ermiasmaz8069@gmail.com>
2025-06-04 18:59:57 +0300

    new_add

--- Laundry/Laundry/deployment_settings.py
+++ Laundry/Laundry/deployment_settings.py
@@ -30,7 +30,7 @@ STORAGES = {
 
 DATABASES = {
     'default': dj_database_url.config(
-        default=os.environ['DATABASE_URL'],
+        default='postgresql://laundry_s6aq_user:QAq4MNwi4Er8oDGkBtncjhJe2ddfRT1E@dpg-d0vk71p5pdvs738jvhsg-a/laundry_s6aq',
         conn_max_age=600
     )
 }
