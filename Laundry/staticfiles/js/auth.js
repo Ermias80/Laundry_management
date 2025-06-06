3cb2520010dbc2dfed069a9ffbfc24e155999ec2
Ermias80 <ermiasmaz8069@gmail.com>
2025-06-06 22:47:57 +0300

    a

---
+++ Laundry/staticfiles/js/auth.js
@@ -0,0 +1,8 @@
+function showSignIn() {
+    document.getElementById('login').style.display = 'flex';
+    document.getElementById('register').style.display = 'none';
+}
+function showSignUp() {
+    document.getElementById('login').style.display = 'none';
+    document.getElementById('register').style.display = 'flex';
+}
