82f070b42e5a88e846b478359f052885f4f6263e
Ermias80 <ermiasmaz8069@gmail.com>
2025-06-08 02:25:28 +0300

    js

--- Laundry/staticfiles/js/index.js
+++
@@ -1,7 +0,0 @@
-function redirectToSignIn() {
-    window.location.href = 'sign_up_or_sign_in.html'; // URL for the login page
-}
-
-function redirectToSignUp() {
-    window.location.href = 'sign_up_or_sign_in.html'; // URL for the registration page
-}
