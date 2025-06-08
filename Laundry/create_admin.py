f5f96d98b600bb52b2fac15b2a1d52e1a1564feb
Ermias80 <ermiasmaz8069@gmail.com>
2025-06-08 11:18:04 +0300

    a

--- Laundry/create_admin.py
+++ Laundry/create_admin.py
@@ -15,6 +15,6 @@ password = "12345?"
 
 if not User.objects.filter(username=username).exists():
     User.objects.create_superuser(username=username, email=email, password=password)
-    print("✅ Superuser created.")
+    print("Superuser created.")
 else:
-    print("⚠️ Superuser already exists.")
+    print("Superuser already exists.")
