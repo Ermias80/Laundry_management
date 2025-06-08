b15c1a9c1a6f9266b1d08c8a3e389c499c25b327
Ermias80 <ermiasmaz8069@gmail.com>
2025-06-08 11:40:00 +0300

    s

--- Laundry/Laundry_p/settings.py
+++ Laundry/Laundry_p/settings.py
@@ -115,3 +115,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+ALLOWED_HOSTS = ['laundry-management-62sq.onrender.com']
+CSRF_TRUSTED_ORIGINS = ['https://laundry-management-62sq.onrender.com']
